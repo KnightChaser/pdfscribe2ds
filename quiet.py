45ac1b5eff617f3c4c80ac9e628992383e8917ca
KnightChaser <agerio100@naver.com>
2025-10-31 17:11:46 +0900

    Chore: Adjust quiet.py's environment variable settings

--- quiet.py
+++ quiet.py
@@ -10,11 +10,14 @@ os.environ.setdefault("TQDM_DISABLE", "1")
 os.environ.setdefault("TOKENIZERS_PARALLELISM", "false")
 os.environ.setdefault("TRANSFORMERS_NO_ADVISORY_WARNINGS", "1")
 os.environ.setdefault("RAY_DEDUP_LOGS", "1")
-os.environ.setdefault("TORCH_CPP_LOG_LEVEL", "ERROR")
 os.environ.setdefault("NCCL_DEBUG", "ERROR")
-os.environ.setdefault("GLOG_minloglevel", "2")
 os.environ.setdefault("BITSANDBYTES_NOWELCOME", "1")
 os.environ.setdefault("PYTHONWARNINGS", "ignore")
+os.environ.setdefault("GLOG_minloglevel", "2")
+os.environ.setdefault("TORCH_CPP_LOG_LEVEL", "ERROR")
+os.environ["GLOG_minloglevel"] = "2"
+os.environ["TORCH_CPP_LOG_LEVEL"] = "ERROR"
+
 
 # Python-level warnings & logging
 warnings.filterwarnings("ignore")
